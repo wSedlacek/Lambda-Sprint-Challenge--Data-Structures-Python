768206499525bf92d19c4159592103569351280a
William Sedlacek <wsedlacekc@gmail.com>
2020-02-28 10:50:43 -0600

    Ring Buffer
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,20 +1,29 @@
+from typing import Union
 from doubly_linked_list import DoublyLinkedList
 
 
 class RingBuffer:
-    def __init__(self, capacity):
+    def __init__(self, capacity: int):
         self.capacity = capacity
-        self.current = None
         self.storage = DoublyLinkedList()
+        self.current = self.storage.head
 
-    def append(self, item):
-        pass
+    def append(self, item: Union[str, int]):
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.tail
+        else:
+            self.current = self.current.next or self.storage.head
+            self.current.value = item
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        self.tracker = self.storage.head
+        while self.tracker:
+            list_buffer_contents.append(self.tracker.value)
+            self.tracker = self.tracker.next
 
         return list_buffer_contents
 
